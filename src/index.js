d6e27d81f37838c0c257aa243b47ac8ff5eccc1e
LeonMesquita <leonmesquita16@gmail.com>
2022-08-09 12:37:33 -0300

    feature signup concluida

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 //import urlRouter from "./routers/urlsRouter.js";
 //import userRouter from "./routers/userRouter.js";
-
+import authRouter from "./routers/authRouter.js";
 dotenv.config();
 
 const app = express();
@@ -13,6 +13,9 @@ app.use(cors());
 
 //app.use(userRouter);
 //app.use(urlRouter);
+app.use(authRouter);
+
+
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>  console.log("Server running on port " + process.env.PORT));
+app.listen(PORT, ()=>  console.log("Server running on port " + PORT));
