9228b9099f90a99ddfce7ab7415673a02204ef31
Luan Severo <luansevero@icloud.com>
2022-08-10 16:08:44 -0300

    [Refactor/Feat] Putting hashtags in the right route

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
 import timelineRouter from "./timelineRouter.js";
-import hashtagRouter from "./hashtagRouter.js"
 
 
 const router = Router();
 
 router.use(authRouter);
 router.use(timelineRouter);
-router.use(hashtagRouter);
 
 export default router;
--- src/routers/timelineRouter.js
+++ src/routers/timelineRouter.js
@@ -5,9 +5,14 @@ import { getPosts,getUrlData } from "../controllers/postsController.js";
 
 const timelineRouter = Router();
 
+//Publish new post
+timelineRouter.post("/timeline", )
+
 //autenticar get posts
 timelineRouter.get("/posts" , getPosts);
 timelineRouter.post("/urls" , getUrlData);
 
+//Hashtag Timeline
+timelineRouter.get("/hashtag/:hashtag", )
 
 export default timelineRouter;
