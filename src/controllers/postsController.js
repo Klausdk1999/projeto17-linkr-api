48e55c44944ab5b685cb74809e9a5fab926e4aa3
Luan Severo <luansevero@icloud.com>
2022-08-10 15:57:04 -0300

    [Refactor] New controller: Publish

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -33,22 +33,3 @@ export async function getUrlData(req, res) {
 
   }
 } 
-
-export const publishPost = async () => {
-  const { userId } = res.locals;
-  const { url, description } = req.body;
-  const { allHashtagsIds } = res.locals;
-  try{
-    const postId = await postPublish([userId, description, url]);
-    if(allHashtags > 0){
-      for(let i = 0; i < allHashtagsIds.length; i++){
-        const hashtagId = allHashtagsIds[i];
-        await postHashtags([hashtagId, postId]);
-      }
-    }
-    res.sendStatus(200);
-  }catch(error){
-    console.log(`[ERRO] In publishPost controller`);
-    return res.status(500).send(error);
-  }
-}
