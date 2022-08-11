85a69461e6715e63c79c9bd09130c34c75182900
Klaus Kupper <klausdk1999@gmail.com>
2022-08-11 20:26:34 -0300

    fix: posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,4 @@
-//import { postsRepository } from "../repositories/postsRepository.js";
+import { postsRepository } from "../repositories/postsRepository.js";
 import connection from "../setup/database.js";
 import { getLinkPreview, getPreviewFromContent } from "link-preview-js";
 import { deletePostRepository } from "../repositories/deleteRepository.js";
