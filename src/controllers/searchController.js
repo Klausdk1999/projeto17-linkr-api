c7861f0cd64e39c44ea1c20617b9f8dc453233ff
Luan Severo <luansevero@icloud.com>
2022-08-16 16:45:16 -0300

    [Feat] Search with new parameter: following. To know if user follow the other user our no.

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,11 +1,13 @@
 import { searchRepository } from "../repositories/searchRepository.js";
 
 export async function searchUser(req, res){
-    const input= req.params.input;
-    try{
-      const users = await searchRepository.searchUsers(input);
-      return res.status(201).send(users.rows);
-    }catch(e){
-      return res.status(500).send(e);
-    }
+  const { userId } = res.locals;
+  const { searchUsername } = req.body;
+    // try{
+      const { rows:users } = await searchRepository.searchUsers([userId, `${searchUsername}%`]);
+      if(users.length === 0) return res.sendStatus(404);
+      res.status(201).send(users);
+    // }catch(e){
+    //   return res.status(500).send(e);
+    // }
   }
--- src/repositories/searchRepository.js
+++ src/repositories/searchRepository.js
@@ -1,8 +1,24 @@
 import connection from "../setup/database.js";
 
-async function searchUsers(input) {
+async function searchUsers(queryString) {
     
-    return connection.query(`SELECT username,picture_url,id FROM users WHERE to_tsvector(username) @@ to_tsquery('"${input}"":*')`);
+    return connection.query(`
+    SELECT u.username, u.picture_url, u.id,
+    (
+        SELECT EXISTS (
+            SELECT id
+            FROM follows
+            WHERE follower_id = $1
+            AND followed_id = u.id
+        )
+    ) as following
+    FROM users u
+    WHERE REPLACE (u.username, ' ', '') 
+    LIKE REPLACE($2, ' ', '')
+    ORDER BY following DESC
+    `, 
+    queryString
+    );
 }
 
 export const searchRepository = {
