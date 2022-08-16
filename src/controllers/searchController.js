cc8384158cd1436aa403a3798b1147bde960e448
Luan Severo <luansevero@icloud.com>
2022-08-16 16:47:45 -0300

    [Fix] Try and catch

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -3,11 +3,11 @@ import { searchRepository } from "../repositories/searchRepository.js";
 export async function searchUser(req, res){
   const { userId } = res.locals;
   const { searchUsername } = req.body;
-    // try{
+    try{
       const { rows:users } = await searchRepository.searchUsers([userId, `${searchUsername}%`]);
       if(users.length === 0) return res.sendStatus(404);
       res.status(201).send(users);
-    // }catch(e){
-    //   return res.status(500).send(e);
-    // }
+    }catch(e){
+      return res.status(500).send(e);
+    }
   }
