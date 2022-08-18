0c176b2cd7522edef44aadd52e94b3af73d39bc1
Luan Severo <luansevero@icloud.com>
2022-08-18 19:39:04 -0300

    [Fix]Timelines statusCode

--- src/controllers/timelinesController.js
+++ src/controllers/timelinesController.js
@@ -5,7 +5,7 @@ import { findHashtag } from "../repositories/trendingRepository.js";
 
 const getQueryString = (where, page, created) => {
     const offsetSize = page * 10
-    if(created === undefined){
+    if(created === 'undefined'){
         return [where, offsetSize]
     } else {
         return [where, offsetSize, created]
@@ -14,36 +14,38 @@ const getQueryString = (where, page, created) => {
 
 async function getTimelinePosts(req,res){
     const { userId } = res.locals;
-    const { page, created } = req.params;
-    try{
-        const {rows:following} = await followRepository.getFollowings([userId]);
+    const { page, created } = req.query;
 
-        if(following.length === 0) return res.status(404).send("follows");
+    // try{
+        const {rows:following} = await followRepository.getFollowings([userId]);
+        if(following.length === 0) return res.status(404).send({page:"timeline", where:"follows"});
 
         const queryString = getQueryString(userId, page, created)
 
+        console.log(queryString)
+
         const { rows:posts } = await postsRepository.getFriendsPosts(queryString);
-        if(posts.length === 0) return res.status(404).send("posts")
+        if(posts.length === 0) return res.status(404).send({page:"timeline", where:"posts"})
         res.status(200).send(posts)
-    }catch(e){
-        console.log(`[ERRO] In getHashtagPosts Controller`);
-        return res.status(500).send(e);
-    }
+    // }catch(e){
+    //     console.log(`[ERRO] In getHashtagPosts Controller`);
+    //     return res.status(500).send(e);
+    // }
 };
 
 
 const getHashtagPosts = async (req,res) => {
     const { hashtag } = req.params;
-    const { page, created } = req.params;
+    const { page, created } = req.query;
     try{
         const { rows: haveHashtag } = await findHashtag([hashtag]);
-        if(haveHashtag.length === 0) return res.status(404).send("hashtag");
+        if(haveHashtag.length === 0) return res.status(404).send({page:"hashtag", where:"hashtag"});
 
         const queryString = getQueryString(hashtag, page, created)
 
         const { rows: hashtagPosts } = await postsRepository.getHashtagPosts(queryString);
-        console.log(hashtagPosts)
-        if(hashtagPosts.length === 0) return res.status(404).send("posts");
+
+        if(hashtagPosts.length === 0) return res.status(404).send({page:"hashtag", where:"posts"});
 
         res.status(200).send(hashtagPosts);
     }catch(error){
@@ -54,21 +56,23 @@ const getHashtagPosts = async (req,res) => {
 
 async function getUserPosts(req, res){
     const userId = req.params.id;
-    const { page, created } = req.params;
-    try{
+    const { page, created } = req.query;
+    console.log(`Oi`)
+    // try{
         const { rows:haveUserId } = await searchRepository.searchUserById([userId]);
-        if(haveUserId.length === 0) res.status(404).send("Usuario não existe");
+        console.log(haveUserId)
+        if(haveUserId.length === 0) res.status(404).send({page:"user", where:"user"});
 
         const queryString = getQueryString(userId, page, created)
 
       const { rows:posts } = await postsRepository.getUserPosts(queryString);
-      if(posts.length === 0) return res.status(404).send("posts")
+      if(posts.length === 0) return res.status(404).send({page:"user", where:"posts"})
   
       return res.status(200).send(posts);
-    }catch(e){
-      console.log(`[ERRO] In getUserPosts Controller`);
-      return res.status(500).send(e);
-    }
+    // }catch(e){
+    //   console.log(`[ERRO] In getUserPosts Controller`);
+    //   return res.status(500).send(e);
+    // }
   }
   
 
